4b6425ce2d7cfda9e1d2a357ec8112c1abf37c27
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2025-07-28 22:12:39 +0500

    Updated axiosInstance.js

--- connectDb/connectDb.js
+++ connectDb/connectDb.js
@@ -2,5 +2,9 @@ import mongoose from "mongoose";
 mongoose.set("strictQuery", true);
 
 export const connectDB = (url) => {
-  return mongoose.connect(url);
+  try {
+    return mongoose.connect(url);
+  } catch (err) {
+    console.log("mongodb connection error: ", err.message);
+  }
 };
