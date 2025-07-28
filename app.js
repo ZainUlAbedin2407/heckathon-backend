8ef815923af8faabaa2cd1ffa51ba83c109f4d85
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2025-07-29 02:11:17 +0500

    Debugging cors issues

--- app.js
+++ app.js
@@ -20,11 +20,28 @@ dotenv.config();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+// app.use(cors());
 // {
 //     origin: "http://localhost:5173",
 //     credentials: true,
 //   }
+const allowedOrigins = [
+  "http://localhost:5173",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
+
 app.use(helmet());
 // app.use(mongoSanitize());
 app.use(hpp());
