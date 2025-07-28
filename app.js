e0ff3f77d79b1cd13cef4c01924a9cfd92788cab
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2025-07-28 22:05:23 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -20,12 +20,11 @@ dotenv.config();
 // Middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+app.use(cors());
+// {
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//   }
 app.use(helmet());
 // app.use(mongoSanitize());
 app.use(hpp());
