f6c69ca001e2fb2993788d1cfeeb31468587f6d4
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2025-07-28 21:28:49 +0500

    testing

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -4,6 +4,6 @@ import { verifyAdmin } from "../middlewares/jwt.js";
 
 const router = express.Router();
 
-router.get("/admin-dashboard", verifyAdmin, adminDashboard);
+router.get("/admin-dashboard", adminDashboard);
 
 export default router;
