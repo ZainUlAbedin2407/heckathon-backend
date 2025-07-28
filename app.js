f463accdca6732ba25dd407c15d9f2ae63d2433b
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2025-07-28 21:06:29 +0500

    Updated app.js

--- app.js
+++ app.js
@@ -51,8 +51,8 @@ const connect = async () => {
 
 connect();
 
-const PORT = process.env.PORT || 3000;
 if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
     console.log("Server is running on port", PORT);
   });
