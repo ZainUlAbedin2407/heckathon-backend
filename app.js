6ef0ffead030429451f7f1c8a154ea91dbce8819
ZainUlAbedin2407 <zainulabedin2407@gmail.com>
2025-07-31 16:02:56 +0500

    solving cors issues

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(express.json());
 //   }
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://zainulabedin2407-heckathon.vercel.app/"
+  "https://zainulabedin2407-heckathon.vercel.app"
 ];
 
 app.use(
